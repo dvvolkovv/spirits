200390f1e4a7cef3ee1a155d96c976e2ec7c75ec
dvvolkovv <dvvolkovv@gmail.com>
2025-11-17 15:30:05 +0700

    Updated PhoneInput.tsx

--- src/components/onboarding/PhoneInput.tsx
+++ src/components/onboarding/PhoneInput.tsx
@@ -162,7 +162,7 @@ const PhoneInput: React.FC<PhoneInputProps> = ({ onSubmit, onDemoClick, isLoadin
               )}
             </button>
 
-            {/* Demo Mode Button */}
+           <!-- {/* Demo Mode Button */}
             <div className="relative">
               <div className="absolute inset-0 flex items-center">
                 <div className="w-full border-t border-gray-300" />
@@ -179,7 +179,8 @@ const PhoneInput: React.FC<PhoneInputProps> = ({ onSubmit, onDemoClick, isLoadin
             >
               <span>Попробовать без регистрации</span>
             </button>
-          </div>
+          </div>-->
+            
         </form>
       </div>
 
